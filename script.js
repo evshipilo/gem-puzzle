8a0971f3693492a82cccd53ba4681a8425af6bff
Yauheni Shypila <evshipilo@gmail.com>
2020-04-02 21:50:30 +0300

    add: generate DOM

--- script.js
+++ script.js
@@ -2,22 +2,21 @@
 const body = document.querySelector('body');
 const arrOfItemValues = [];
 const numOfItems = 16;
-const arrOfItems = [];
+let arrOfItems = [];
 function createPuzzleDiv() {
   body.insertAdjacentHTML('afterbegin', '<div class="puzzle"></div>');
 }
 createPuzzleDiv();
 const puzzleDiv = document.querySelector('.puzzle');
 // response puzzle
-let puzzleWidth = getComputedStyle(puzzleDiv).width.split('px')[0];
+let [puzzleWidth] = getComputedStyle(puzzleDiv).width.split('px');// деструктурир присвивание
 let widthOfItems = puzzleWidth / numOfItems;
 puzzleDiv.style.height = `${+puzzleWidth}px`;
 window.onresize = function () {
-  puzzleWidth = getComputedStyle(puzzleDiv).width.split('px')[0];
+  [puzzleWidth] = getComputedStyle(puzzleDiv).width.split('px');
   puzzleDiv.style.height = `${+puzzleWidth}px`;
   widthOfItems = puzzleWidth / numOfItems;
 };
-
 
 function makeStraightArrOfItemValues() {
   for (let i = 1; i <= numOfItems; i += 1) {
@@ -27,16 +26,17 @@ function makeStraightArrOfItemValues() {
 function makeRandomArr() {
   return Math.random() - 0.5;
 }
-function hideLastItem() {
-  const hideItemNumber = arrOfItemValues.length;
-  document.querySelector(`.item${hideItemNumber}`).classList.add('hidden');
-}
 function setPositionOfItem() {
-  for
-    const itemDiv = item;
-    itemDiv.style.width = `${widthOfItems}px`;
-    itemDiv.style.height = `${widthOfItems}px`;
-  });
+  let j = 0;
+  let k = 0;
+  for (let i = 0; i < arrOfItems.length; i += 1) {
+    j += 1;
+    arrOfItems[i].style.width = `${widthOfItems}px`;
+    arrOfItems[i].style.height = `${widthOfItems}px`;
+    arrOfItems[i].style.top = `${k * widthOfItems}px`;
+    arrOfItems[i].style.left = `${j * widthOfItems}px`;
+    if (j === Math.sqrt(numOfItems)) { j = 0; k += 1; }
+  }
 }
 
 function generatePuzzleRandome() {
@@ -46,7 +46,9 @@ function generatePuzzleRandome() {
     puzzleDiv.insertAdjacentHTML('afterbegin', `
 <div class="item${arrOfItemValues[i]}">${arrOfItemValues[i]}</div>`);
   }
-  hideLastItem();
+  document.querySelector(`.item${numOfItems}`).classList.add('hidden');
   arrOfItems = document.querySelectorAll('.puzzle div');
+  console.log(arrOfItems);
+  setPositionOfItem();
 }
-// generatePuzzleRandome(9);
+ generatePuzzleRandome();
